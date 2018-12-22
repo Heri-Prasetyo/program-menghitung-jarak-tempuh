2fca7900039919d39f7b2a14c07732039136ff91
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 09:54:33 +0700

    Add files via upload

---
+++ Program C++ Menghitung Jarak Tempuh by Heri.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int v,t,s;
+
+cout<<"PROGRAM MENGHITUNG JARAK TEMPUH"<<endl;
+
+cout<<"Masukan jumlah kecepatan:";
+cin>>v;
+cout<<"Masukan waktu tempuh    :";
+cin>>t;
+s=v*t;
+cout<<"Maka Jarak Tempuhnya Adalah = "<<s;
+
+
+
+
+
+
+return 0;
+}
